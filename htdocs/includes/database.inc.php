4eb1fa4d9e8e1a51afc1030b0ac2c4a50251ce85
LiamMean41 <102616188+LiamMean41@users.noreply.github.com>
2024-05-06 23:39:57 +0100

    Delete htdocs/includes/database.inc.php

--- htdocs/includes/database.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$connect = mysqli_connect('sql309.infinityfree.com', 'if0_36106638', 'w84Vvp8xJYs', 'if0_36106638_ddlk');
-
-if (mysqli_connect_errno()) {
-    exit('Failed to connect to MySQL: ' . mysqli_connect_error());
-}
